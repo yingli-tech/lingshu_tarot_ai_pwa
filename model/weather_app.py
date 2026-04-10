78180707b56b96b0cfdf90fb3cafd4d79899c8ba
yingli-tech <yingli2026@u.northwestern.edu>
2026-04-10 17:28:58 -0500

    chore: remove unused files

--- model/weather_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Your Personal Weather Assitant")
-city = st.text_input("Please type in the city name")
-if city:
-    st.write(f"Checking the weather for {city}...")
